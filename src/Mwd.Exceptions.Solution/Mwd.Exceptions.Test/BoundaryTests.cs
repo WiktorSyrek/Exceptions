b6ae1a97274c64b574ff528145a10dfb4f23ff91
moerwald <andreas.mewald@gmail.com>
2018-12-23 16:57:16 +0100

    Added test project.

---
+++ src/Mwd.Exceptions.Solution/Mwd.Exceptions.Test/BoundaryTests.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Mwd.Exceptions.Test
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class BoundaryTests
+    {
+        [Test]
+        public void CatchDoesntPopulateException()
+        {
+            Assert.DoesNotThrow(
+                () => Boundary.CatchAll(() => throw new Exception("Test")));
+        }
+
+        [Test]
+        public void CatchCallsExceptionHappened()
+        {
+            bool exceptionHappenedCalled = false;
+            Boundary.CatchAll(() => throw new Exception("Test"), e => exceptionHappenedCalled = true);
+            Assert.That(exceptionHappenedCalled, Is.True);
+        }
+    }
+}
